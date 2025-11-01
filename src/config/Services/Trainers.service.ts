3c88033ba7a76ab0203488cead961e59c9417354
Altraaa <aldimks7464@gmail.com>
2025-11-01 15:23:31 +0800

    Fix: Route Trainer Service

--- src/config/Services/Trainers.service.ts
+++ src/config/Services/Trainers.service.ts
@@ -1,16 +1,16 @@
 import { ITrainer } from "../Models/Trainer";
 import { ApiRequest } from "./Api.service";
 
-export const ApiTrainers = {
+export const ApiTrainer = {
   getAll: (): Promise<ITrainer[]> =>
-    ApiRequest({ url: "/trainers", method: "GET" }),
+    ApiRequest({ url: "/trainer", method: "GET" }),
   getById: (id: number): Promise<ITrainer> =>
-    ApiRequest({ url: `/trainers/${id}`, method: "GET" }),
+    ApiRequest({ url: `/trainer/${id}`, method: "GET" }),
   create: (data: Partial<ITrainer>): Promise<ITrainer> =>
     ApiRequest({ url: "/trainers", method: "POST", body: data }),
   update: (id: number, data: Partial<ITrainer>): Promise<ITrainer> =>
     ApiRequest({
-      url: `/trainers/${id}`,
+      url: `/trainer/${id}`,
       method: "POST",
       body: { ...data, _method: "PUT" },
     }),
@@ -19,13 +19,13 @@ export const ApiTrainers = {
     data: Partial<ITrainer>
   ): Promise<{ message: string }> =>
     ApiRequest({
-      url: `/trainers/${id}/update-password`,
+      url: `/trainer/${id}/update-password`,
       method: "POST",
       body: { ...data, _method: "PUT" },
     }),
   delete: (id: number): Promise<{ message: string }> =>
     ApiRequest({
-      url: `/trainers/${id}`,
+      url: `/trainer/${id}`,
       method: "POST",
       body: { _method: "DELETE" },
     }),
